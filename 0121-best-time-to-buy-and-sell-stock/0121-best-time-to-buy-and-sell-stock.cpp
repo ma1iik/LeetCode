d60c7bb03fdfe25fd28a0b98107f068cfc750d04
ma1iik <108078307+ma1iik@users.noreply.github.com>
2024-11-01 03:35:39 +0100

    Time: 0 ms (100.00%), Space: 96.1 MB (89.56%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int minprice = INT_MAX;
+        int min_p = INT_MAX;
         int profit = 0;
-        for (const auto& price : prices) {
-            minprice = min(price, minprice);
-            profit = std::max(profit, price - minprice);
+        for (auto price : prices) {
+            min_p = min(price, min_p);
+            if (price - min_p > profit)
+                profit = price - min_p;
         }
         return profit;
     }
